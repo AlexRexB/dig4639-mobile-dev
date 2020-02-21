0614cc624bc52ad5aa08c1ee7bcd6e54bdc48725
AlexRexB <alexrexb@gmail.com>
2020-02-21 10:59:49 -0500

    message

---
+++ labs/3/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from "./components/card/index.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Card contents="This is a card!"></Card>
+    </div>
+  );
+}
+
+export default App;
